85db0f3d05792e5b0c820593e49c55b9cffbee76
markslingerland <markslingerland@hotmail.com>
2019-01-15 22:38:48 +0100

    -

---
+++ server.js
@@ -0,0 +1,39 @@
+var port = process.env.PORT || 8000;
+
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import logger from './core/logger/app-logger'
+import morgan from 'morgan'
+import config from './core/config/config.dev'
+import cars from './routes/cars.route'
+import connectToDb from './db/connect'
+var http = require('http');
+
+logger.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
+
+// connectToDb();
+
+const app = express();
+
+var server = http.createServer(app);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev", { "stream": logger.stream }));
+
+app.use('/cars', cars);
+
+//Index route
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint!');
+});
+
+server.listen(port, () => {
+    logger.info('server started - ', port);
+});
