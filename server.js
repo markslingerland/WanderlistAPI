46db30cede45992e272d86863281fb9582c15853
markslingerland <markslingerland@hotmail.com>
2019-01-15 17:42:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import logger from './core/logger/app-logger'
-import morgan from 'morgan'
-import config from './core/config/config.dev'
-import cars from './routes/cars.route'
-import connectToDb from './db/connect'
+var http = require('http');
 
-const port = process.env.PORT || 1337;
-logger.stream = {
-    write: function(message, encoding){
-        logger.info(message);
-    }
-};
+var server = http.createServer(function(request, response) {
 
-connectToDb();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev", { "stream": logger.stream }));
-
-app.use('/cars', cars);
-
-//Index route
-app.get('/', (req, res) => {
-    res.send('Invalid endpoint!');
 });
 
-app.listen(port, () => {
-    logger.info('server started - ', port);
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
