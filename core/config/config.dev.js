9bd868672cbb2c36ad7527a272c3568f4622a936
markslingerland <markslingerland@hotmail.com>
2019-01-15 17:28:15 +0100

    Update config.dev.js

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -7,6 +7,6 @@ config.logFileName = 'app.log';
 config.dbHost = process.env.dbHost || 'localhost';
 config.dbPort = process.env.dbPort || '27017';
 config.dbName = process.env.dbName || 'trains';
-config.serverPort = process.env.PORT || 3000;
+config.serverPort = process.env.PORT || 1337;
 
 export default config;
